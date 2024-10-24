da050ce24f2f2c91b9a28592ea8bd1f802e6705f
lfdou <2648142550@qq.com>
2024-10-24 13:32:37 +0800

    同步更新111

--- functions/random.js
+++ functions/random.js
@@ -8,16 +8,58 @@ export async function onRequest(context) {
       next, // used for middleware or to fetch assets
       data, // arbitrary space for passing data between middlewares
     } = context;
-    const list = await env.img_url.list();
+    const requestUrl = new URL(request.url);
+    const protocol = requestUrl.protocol;
+    const domain = requestUrl.hostname;
+    const port = requestUrl.port;
+    let allRecords = [];
+    let cursor = null;
+    do {
+        const records = await env.img_url.list({
+            limit: 1000,
+            cursor,
+        });
+        allRecords.push(...records.keys);
+        cursor = records.cursor;
+    } while (cursor);
     if (env.AllowRandom != "true") {
         return new Response(JSON.stringify({ error: "Random is disabled" }), { status: 403 });
     }
-    if (list.keys.length == 0) {
+    if (allRecords.length == 0) {
         return new Response(JSON.stringify({}), { status: 200 });
     } else {
-        const randomIndex = Math.floor(Math.random() * list.keys.length);
-        const randomKey = list.keys[randomIndex];
-        const randomUrl = '/file/' + randomKey.name;
-        return new Response(JSON.stringify({ url: randomUrl }), { status: 200 });
+        const randomIndex = Math.floor(Math.random() * allRecords.length);
+        const randomKey = allRecords[randomIndex];
+        const randomPath = '/file/' + randomKey.name;
+        let randomUrl = randomPath;
+        const randomType = requestUrl.searchParams.get('type');
+        const resType = requestUrl.searchParams.get('form');
+        // if param 'type' is set to 'url', return the full URL
+        if (randomType == 'url') {
+            if (port) {
+                randomUrl = protocol + '//' + domain + ':' + port + randomPath;
+            } else {
+                randomUrl = protocol + '//' + domain + randomPath;
+            }
+        }
+        // if param 'type' is set to 'img', return the image
+        if (randomType == 'img') {
+            // Return an image response
+            randomUrl = protocol + '//' + domain + ':' + port + randomPath;
+            let contentType = 'image/jpeg';
+            return new Response(await fetch(randomUrl).then(res => {
+                contentType = res.headers.get('content-type');
+                return res.blob();
+            }), {
+                headers: contentType ? { 'Content-Type': contentType } : { 'Content-Type': 'image/jpeg' },
+                status: 200
+            });
+        }
+        
+        if (resType == 'text') {
+            return new Response(randomUrl, { status: 200 });
+        } else {
+            return new Response(JSON.stringify({ url: randomUrl }), { status: 200 });
+        }
     }
 }
